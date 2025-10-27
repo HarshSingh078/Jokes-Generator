8f7bc30adbf7e1cf5078cb588b931a62d44f5aa3
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-10-27 19:36:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Jokes Generator using React and Joke API</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Joke.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+function Joke() {
+    const [joke , setJoke] = useState("")
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Joke
